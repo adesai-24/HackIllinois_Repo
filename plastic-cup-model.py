6524af7970ece4d9e4ba3b6a9efd0eba3ff3dd47
Pranav Adya <pranavadya@gmail.com>
2025-03-01 01:53:33 -0600

    push

--- plastic-cup-model.py
+++ plastic-cup-model.py
@@ -29,7 +29,7 @@ while True:
                 confidence = box.conf[0]  # Confidence score
                 label = f"Cup {confidence:.2f}"
                 # Draw bounding box and label
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 
                             0.5, (0, 255, 0), 2)
 
