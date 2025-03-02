5a5aee7d0f64cc2810e4249018c564d5418dbc1e
Pranav Adya <pranavadya@gmail.com>
2025-03-02 01:35:47 -0600

    test

--- PID/pid_cam.py
+++ PID/pid_cam.py
@@ -12,12 +12,12 @@ KP_TURN = 0             # Proportional gain for steering
 ERROR_DEADBAND = 20          # Pixel threshold below which cup is considered centered
 MAX_TURN_SPEED = 50          # Maximum steering command (degrees)
 FORWARD_SPEED = 50           # Forward motor speed when driving forward
-FORWARD_DURATION = 0.8     # Duration (seconds) to drive forward when cup detected
+FORWARD_DURATION = 0.81     # Duration (seconds) to drive forward when cup detected
 
 FRAME_WIDTH = 640            # Camera frame width (pixels)
 FRAME_HEIGHT = 480           # Camera frame height (pixels)
 
-# Initialize Components
+# Initialize Componentss
 px = Picarx()
 model = YOLO("yolov5su.pt")  # Using the improved 'u' model
 
